1df240e3d83e6d259dbca189079d3f1c7df1a159
damatoaj <mr.arthurdamato@gmail.com>
2021-09-01 13:52:45 -0700

    added alt tags

--- src/Components/Partials/Footer.js
+++ src/Components/Partials/Footer.js
@@ -8,16 +8,16 @@ const Footer = () => {
             <Container id="footer">
                     <Row>
                         <a href="https://github.com/damatoaj" target="_blank">
-                            <Image className="socialMedia" src="/images/github.svg" />
+                            <Image className="socialMedia" src="/images/github.svg" alt="GitHub"/>
                         </a>
                         <a href="https://www.instagram.com/thundrousprophet/?hl=en" target="_blank">
-                            <Image className="socialMedia" src="/images/instagram_icon.svg" />
+                            <Image className="socialMedia" src="/images/instagram_icon.svg" alt="Instagram" />
                         </a>
                         <a href="https://www.linkedin.com/in/arthur-d-amato-45b6b169/" target="_blank">
-                            <Image  className="socialMedia" src="/images/linked_in_logo.svg"  />
+                            <Image  className="socialMedia" src="/images/linked_in_logo.svg" alt="LinkedIn" />
                         </a>
                         <a href="https://arthurdamato.wordpress.com/" target="_blank">
-                            <Image className="socialMedia" src="/images/wordpress_logo.svg"  />
+                            <Image className="socialMedia" src="/images/wordpress_logo.svg" alt="WordPress" />
                         </a>
                     </Row>    
             </Container>

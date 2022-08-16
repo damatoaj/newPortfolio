346b778288b5f1ad057152f9d8fa9eca777b52fa
damatoaj <mr.arthurdamato@gmail.com>
2022-08-16 08:12:18 -0500

    Updated volume-tracker's URL

--- src/Components/Statics/MyProjects.js
+++ src/Components/Statics/MyProjects.js
@@ -23,7 +23,7 @@ const MyProjects = [
         synopsis: 'A full-stack application using an SQL database that allows users to track their volume (heart rate multiplied by minutes), and graph the data over time.',
         image: '/images/volume_tracker.png',
         technologies: ['PostGres', 'HTML', 'CSS', 'JavaScript', 'React-Bootstrap', 'Next.js'],
-        link: 'https://cardio-volume-tracker.herokuapp.com/',
+        link: 'https://volume-tracker-ga.herokuapp.com/',
         nextSteps: ''
     },
     {
